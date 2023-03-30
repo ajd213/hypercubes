4a956d7a4e76c06490a9cf5456acf3510eff491a
Alex Duthie <alexduthie.1@gmail.com>
2023-03-30 11:04:15 +0300

    Added DFS algorithm for hypercubes to functions.c

--- functions.c
+++ functions.c
@@ -5,6 +5,7 @@
 #include <gsl/gsl_rng.h> 
 #include "functions.h"
 
+gsl_rng *RNG;
 
 // push site to the stack
 int push(stack *s, ul site)
@@ -123,4 +124,67 @@ void populate_sites_XXZ(ul *sites, ul N, int UP)
             counter++;
         }
     }
+}
+
+/*
+ * Function:  DFS_hypercube
+ * --------------------
+ * grows a percolation cluster on a hypercube with a depth-first search:
+ *
+ *  s: a pointer to a stack
+ *  visited: an array of bools of length NH, to record whether each site has been visited
+ *  p: the percolation strength. 0 <= p <= 1
+ *  N: the dimension of the hypercube. (E.g. N=3 is a regular cube.)
+ *  start_state: which site on the hypercube to grow the cluster from
+ *  error: a pointer to an error flag, in case something goes wrong
+ *
+ *  returns: the size of the cluster. I.e., the number of sites visited by the DFS algorithm.
+ */
+ul DFS_hypercube(stack *s, bool visited[], float p, ul N, ul start_state, int *error)
+{
+    ul size = 0; // cluster size
+    ul u, v;
+
+    if (s->top != 0)
+    {
+        printf("Error! Stack not empty!\n");
+        *error = -1; 
+        return 0;
+    }
+    push(s, start_state);
+
+    while (s->top > 0)
+    {
+
+        u = pop(s, error);
+        if (*error == -1)
+        {
+            // error flag has been set, so return.
+            return 0;
+        }
+
+        if (visited[u])
+        {
+            continue;
+        }
+        visited[u] = true;
+        size++;
+
+        for (ul i = 0; i < N; i++)
+        {
+            // flip the ith bit
+            v = u ^ (1UL << i);
+
+            if (!visited[v] && (gsl_rng_uniform(RNG) < p))
+            {
+                if (push(s, v) == 1) 
+                { 
+                    // stack error!
+                    *error = -1; 
+                    return 0;
+                }
+            }
+        }
+    }
+    return size;
 }
