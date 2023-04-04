5282f86d30ab8649a42162bf2db2dd620425f9cc
Alex Duthie <alexduthie.1@gmail.com>
2023-04-04 15:01:51 +0300

    Analysis script higher quality. Commented.

--- analysis/percolation_transition.py
+++ analysis/percolation_transition.py
@@ -1,4 +1,10 @@
-# here write code to produce collapsed plot demonstrating transiton at 1/N
+""" An example analysis script. Here, we locate the percolation transition at p_c = 1/N using a scale collapse.
+
+The essence of this plot is that if the percolation transition occurs at 1/N in the thermodynamic limit,
+then if we plot a measure of percolation (such as the mean cluster size, S) against pN, then the data
+should collapse around a common transition point of pN = 1.
+
+"""
 
 import hypercubes
 import numpy as np
@@ -14,16 +20,18 @@ DATA_PATH = "./plotting_data/"
 if not os.path.exists(DATA_PATH):
     os.makedirs(DATA_PATH)
 
-"""Plot the mean/maximum cluster size against p."""
-
 
 def main():
     fig, ax = plt.subplots(nrows=2, ncols=2, figsize=[8, 8], sharey=True)
     ax = ax.flatten()
 
+    # grid of percolation concentrations, between 0 (non-percolating) and 1 (percolating).
     p_min = 0
     p_max = 1
     N_p = 101
+    plist = np.linspace(p_min, p_max, N_p)
+
+    # number of realisations to use
     NR = 100
 
     # which system sizes to use
@@ -34,64 +42,55 @@ def main():
     clist = plt.cm.viridis_r(Nlist_numbers)
 
 
+    #####################
+    ### PLOT THE DATA ###
+    #####################
+
     for Ni, N in enumerate(Nlist):
-        S, max = s_with_p(N, NR, p_min, p_max, N_p)
-        plist = np.linspace(p_min, p_max, N_p)
+        S, max = s_with_p(N, NR, plist)
 
         ax[0].plot(plist, max/(2**N), '^-',c=clist[Ni], markersize='3', label=f"${N}$")
         ax[1].plot(plist, S/(2**N), 'o-',c=clist[Ni], markersize='3')
-        # ax[1].plot(plist, Sp/(2**N), '--', c=clist[Ni])
-
         ax[2].plot(plist*N, max/(2**N), '^-',c=clist[Ni], markersize='3')
         ax[3].plot(plist*N, S/(2**N), 'o-',c=clist[Ni], markersize='3')
-        # ax[3].plot(plist*N, Sp/(2**N), '--', c=clist[Ni])
+
+
+    #####################
+    ###  FORMAT PLOT  ###
+    #####################
 
     ax[2].set_xlim([0,5])
     ax[3].set_xlim([0,10])
+
     ax[2].axvline(1,linestyle='--', c='black')
     ax[3].axvline(1,linestyle='--', c='black')
 
     ax[0].set_ylabel("$\mathrm{max}(s)/N_\mathcal{H}$")
-    ax[1].set_ylabel("$(S/S')/N_\mathcal{H}$")
+    ax[1].set_ylabel("$S/N_\mathcal{H}$")
     ax[2].set_ylabel("$\mathrm{max}(s)/N_\mathcal{H}$")
-    ax[3].set_ylabel("$(S/S')/N_\mathcal{H}$")
+    ax[3].set_ylabel("$S/N_\mathcal{H}$")
 
     ax[0].set_xlabel("$p$")
     ax[1].set_xlabel("$p$")
-
     ax[2].set_xlabel("$pN$")
     ax[3].set_xlabel("$pN$")
 
-
-
-    props = dict(boxstyle='round', facecolor='white', alpha=0)
-
-    ax[1].text(0.1, 0.9, "$S$",  fontsize=18, verticalalignment='top', bbox=props)
-    ax[1].text(0.63, 0.9, "$S'$",  fontsize=18, verticalalignment='top', bbox=props)
-
-    ax[3].text(1.75, 0.9, "$S$",  fontsize=18, verticalalignment='top', bbox=props)
-    ax[3].text(8.3, 0.9, "$S'$",  fontsize=18, verticalalignment='top', bbox=props)
-
-
-
     ax[0].legend(loc=[0.8, 0.4], handlelength=0.2, columnspacing=0.5, handletextpad=0.2, framealpha=0)
-
 
     plt.subplots_adjust(right=0.95, top=0.99, left=0.11, bottom=0.1, wspace=0.2, hspace=0.25)
     plt.savefig("hypercube_percolation.pdf")
 
 
 
-
-def s_with_p(N, NR, p_min, p_max, N_p):
-    plist = np.linspace(p_min, p_max, N_p)
+def s_with_p(N, NR, plist):
+    """ Create lists of the mean cluster size (Slist) and maximum cluster size."""
+    N_p = len(plist)
 
     Slist = np.zeros(N_p)
     max_sizes = np.zeros(N_p)
 
     for pi, p in enumerate(plist):
-        cs = get_clusters(N, int(NR), p, DATA_PATH)
-
+        cs = get_clusters(N, NR, p, DATA_PATH)
         Slist[pi] = distributions.S(cs)
         max_sizes[pi] = np.max(cs)
 
@@ -99,12 +98,13 @@ def s_with_p(N, NR, p_min, p_max, N_p):
 
 
 def get_clusters(N, NR, p, data_path):
+    """ Attempt to load cluster data, else generate and save it."""
     name = f"clusters_N{N}_NR{NR}_p{p:.4f}.npy"
 
     try:
         clusters = np.load(data_path + name)
     except FileNotFoundError:
-        clusters = hypercubes.clusters(N, NR, p)
+        clusters = hypercubes.clusters(int(N), int(NR), p)
         np.save(data_path + name, clusters)
     return clusters
 
