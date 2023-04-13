48ab8a2850797f89befa154643e1bc91d4b02fa6
Alex Duthie <alexduthie.1@gmail.com>
2023-04-13 14:08:32 +0300

    Initial commit for PXP
    Have copied the hypercube functions into the src/ directory, and added a main.c file. I will work on the source code without Python
    integration for the meantime, to allow for easy compilation and testing.
    
    TODO:
    
    * Add DFS support for the Fibonacci cube
    * Add functions to generate eigenvalues/eigenvectors for both the hypercube and Fib. cube.
    * (Later on:) integrate all fully into the Python module, and remove the src directory afterwards.

---
+++ src/hypercube_functions.h
@@ -0,0 +1,21 @@
+typedef unsigned long ul;
+typedef struct stack
+    {
+        ul *sites;
+        ul length;
+        ul top;
+        ul NH;
+    }
+stack;
+
+extern gsl_rng *RNG;
+
+int push(stack *s, ul site);
+ul pop(stack *s, int *error);
+ul intpower(ul base, ul exponent);
+ul binomialCoeff(ul n, ul k);
+ul index_site(ul *sites, ul site, ul left, ul right, int *idx_flag);
+void reset_visited(bool visited[], ul length);
+void populate_sites_XXZ(ul *sites, ul N, int UP);
+ul DFS_hypercube(stack *s, bool visited[], float p, ul N, ul start_state, gsl_rng *RNG, int *error);
+ul *clusters_hypercube(ul N, ul NR, float p, int *error);
