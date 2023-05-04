3496a19a550258472ce4d9d6e2d61237759d90b5
Alex Duthie <alexduthie.1@gmail.com>
2023-05-04 16:00:09 +0300

    Tidied up functions.h

--- src/functions.h
+++ src/functions.h
@@ -15,18 +15,30 @@ typedef struct stack
     }
 stack;
 
+/* Stack functions */
 int push(stack *s, ul site);
 ul pop(stack *s, int *error);
-ul intpower(ul base, ul exponent);
-ul binomialCoeff(ul n, ul r);
-ul index_site(ul *sites, ul site, ul left, ul right, int *idx_flag);
+stack *setup_stack(ul NH);
 void reset_visited(bool visited[], ul length);
-void populate_sites_PXP(ul *sites, ul N);
+
+/* Hypercube functions */
 ul DFS_hypercube(stack *s, bool visited[], float p, ul N, ul start_state, gsl_rng *RNG, int *error);
 ul *clusters_hypercube(ul N, ul NR, float p, gsl_rng *RNG, int *error);
-bool check_args(ul N, ul NR, float p);
-ul fibonacci(ul n);
+
+/* PXP functions */
+void populate_sites_PXP(ul *sites, ul N);
 ul *clusters_PXP(ul N, ul NR, float p, gsl_rng *RNG, int *error);
 bool PXP_flip_allowed(ul u, ul i, ul N);
 ul DFS_PXP(stack *s, ul *sites, bool visited[], float p, ul N, ul start_state, gsl_rng *RNG, int *error);
-stack *setup_stack(ul NH);
+
+/* Maths functions */
+ul intpower(ul base, ul exponent);
+ul binomialCoeff(ul n, ul r);
+ul index_site(ul *sites, ul site, ul left, ul right, int *idx_flag);
+ul fibonacci(ul n);
+
+/* Misc functions */
+bool check_args(ul N, ul NR, float p);
+
+
+
