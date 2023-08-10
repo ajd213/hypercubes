6fbe42bd4472fd27697a74fdcd0a74836568000f
Alex Duthie <alexduthie.1@gmail.com>
2023-08-10 14:55:07 +0300

    added numpy import

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup, Extension
+import numpy
 
 module = Extension("hypergraphs", sources = ["hypergraphs.c", "hypercube_functions.c", "pxp_functions.c", "helper_functions.c"],
 libraries = ["m", "gsl", "gslcblas"])
 setup(name = "hypergraphs", 
 version = "1.0", 
 description = "Percolation on hypergraphs", 
+include_dirs=[numpy.get_include()],
 ext_modules = [module])
